4c8d49043c09f657dc2623096650c4a11bac2012
huangjingyuan <837317210@qq.com>
2024-09-07 15:42:24 +0800

    chore: 🔨 添加eslint stylelint prettier husky commitlint cz-git commitizen tailwind 等依赖

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+import pluginPrettierRecommendedConfigs from 'eslint-plugin-prettier/recommended'
+import parserVue from 'vue-eslint-parser'
+
+export default [
+	{
+		ignores: ['node_modules', '**/node_modules', '**/dist'],
+	},
+	{ files: ['**/*.{js,mjs,cjs,ts,vue}'] },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	...pluginVue.configs['flat/essential'],
+	// prettier 默认推荐规则
+	pluginPrettierRecommendedConfigs,
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.es2020,
+				...globals.node,
+			},
+			ecmaVersion: 2020,
+			parser: parserVue,
+			parserOptions: {
+				parser: tseslint.parser,
+			},
+		},
+	},
+	{
+		files: ['**/*.vue'],
+		languageOptions: { parserOptions: { parser: tseslint.parser } },
+	},
+]
